5b6304c6028ecf5bec753cd16aa46d4df918ce2f
rpdecks <rpdecks@gmail.com>
2020-04-09 10:06:46 -0400

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "M", "White", "Nice", "Y"); 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "M", "White", "Nice", "Y");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 11, "F", "Brown", "Nice", "Y");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 15, "F", "Brown", "mean", "N");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "F", "Black", "kind", "Y");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 20, "F", "Black", "kind", "N");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 30, "F", "Brown", "soft", "N");
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 99, "F", "Blue", "happy", "Y");
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "M", "White", "Nice", true); 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "M", "White", "Nice", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 11, "F", "Brown", "Nice", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 15, "F", "Brown", "mean", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "F", "Black", "kind", true);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 20, "F", "Black", "kind", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 30, "F", "Brown", "soft", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 99, "F", "Blue", "happy", true);
