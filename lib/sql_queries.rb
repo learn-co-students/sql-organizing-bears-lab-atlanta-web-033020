ef7bee98b724f761922a18705e24f76994e99288
rpdecks <rpdecks@gmail.com>
2020-04-08 20:46:59 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -18,14 +18,15 @@ def select_youngest_bear_and_returns_name_and_age
   'SELECT name, age FROM bears ORDER BY (age) ASC LIMIT 1;'
 end
 
-# def selects_most_prominent_color_and_returns_with_count
-#   'SELECT COUNT (color) AS value_occurence FROM bears GROUP BY color ORDER BY value_occurence ASC LIMIT 1;'
-# end
+def selects_most_prominent_color_and_returns_with_count
+  'SELECT color, COUNT (color) AS total FROM bears GROUP BY color ORDER BY total DESC LIMIT 1;'
+end
 
 def counts_number_of_bears_with_goofy_temperaments
   'SELECT COUNT (temperament) FROM bears WHERE temperament = "goofy";'
 end
 
 # def selects_bear_that_killed_Tim
-#   'SELECT * FROM bears WHERE (name IS NULL AND alive = 0);'
+#   # 'SELECT * FROM bears WHERE (name IS NULL AND alive = 0);'
+#   # 'SELECT * FROM bears WHERE id = 8;'
 # end
