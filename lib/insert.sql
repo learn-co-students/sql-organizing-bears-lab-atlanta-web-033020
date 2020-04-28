f1a7a1ef144f733b99f9f37bf2da3df14e210cf7
devWRM <mcwen@comcast.net>
2020-04-28 10:17:18 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+
+-- learn spec/insert_spec.rb
+-- 0=false  1=true
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 30, 'M', 'milk chocolate', 'sweet', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 19, 'F', 'tan', 'salty', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 71, 'F', 'green', 'anxious', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 80, 'F', 'caramel', 'hot', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 44, 'F', 'polar', 'peaceful', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 65, 'M', 'pasty', 'bitter', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 121, 'F', 'brown', 'reserved', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 28, 'M', 'brown', 'violent', 0);
+
+
+
+
+
